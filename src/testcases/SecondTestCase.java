db7d96459a02034b440c7e9ae1fbe70fa9f3b960
vinaydhawan <vinay.dhawan@tcs.com>
2016-09-30 18:16:54 +0530

    secodn test case updated

--- src/testcases/SecondTestCase.java
+++ src/testcases/SecondTestCase.java
@@ -14,7 +14,8 @@ public class SecondTestCase {
 	@BeforeClass
 	public static void begin(){
 		System.out.println("*************begining*********************");
-		System.out.println("*************begining of branch concept*********************");
+		System.out.println("*************begining of branch concept****");
+		
 	}
 	
 	@AfterClass
